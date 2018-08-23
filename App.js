b553187b4c2de23404ef3c9d1d9f04c5ac2f5716
Stephen Cronin <stephen.cronin@reaktor.com>
2018-08-23 13:19:34 -0400

    Apply middleware and use plugin for devtools integration

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 
 import reducers from './reducers';
 
+const middleware = [
+  thunk,
+];
+
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(...middleware),
+  ),
 );
 
 import DeckContainer from './containers/DeckContainer';
