4da1b3d9d32f87a7d54f713aa9bc658e8a359690
Stephen Cronin <stephen.cronin@reaktor.com>
2018-08-23 11:50:10 -0400

    Add debug text to stacked pile

--- components/StackedPile/index.js
+++ components/StackedPile/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import CardContainer from '../../containers/CardContainer';
 import EmptyCardSpace from '../EmptyCardSpace';
@@ -11,6 +11,11 @@ export default class StackedPile extends React.Component {
     const { pile } = this.props;
     return (
       <View style={styles.pile}>
+        <View style={styles.debugOverlay} pointerEvents={'none'}>
+          <View style={styles.debugOverlayInner}>
+            <Text style={styles.debugOverlayText}>{pile.length} cards</Text>
+          </View>
+        </View>
         <EmptyCardSpace absolute={pile.length > 0} />
         {pile.slice(pile.length - 1).map((card, index) =>
           <View key={index}>
@@ -26,5 +31,21 @@ const styles = StyleSheet.create({
   pile: {
     flexDirection: 'column',
     overflow: 'hidden',
+  },
+  debugOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  debugOverlayInner: {
+    padding: 3,
+    backgroundColor: 'rgba(255, 255, 255, 0.85)'
+  },
+  debugOverlayText: {
+    fontFamily: 'Menlo-Regular',
+    fontSize: 9,
+    lineHeight: 9,
+    textAlign: 'center',
   },
 });
