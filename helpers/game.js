7be8011ab38d35eaf901fe21d470477be017da30
Stephen Cronin <stephen.cronin@reaktor.com>
2018-08-30 17:09:26 -0400

    Add pad start polyfill

--- helpers/game.js
+++ helpers/game.js
@@ -1,3 +1,20 @@
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
+
 export const displayTime = (seconds) => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds - (seconds * minutes);
