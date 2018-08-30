ad02d67431e231febc8ec0cb41d6115435af522f
Stephen Cronin <stephen.cronin@reaktor.com>
2018-08-30 17:15:25 -0400

    Fixed time display

--- helpers/game.js
+++ helpers/game.js
@@ -17,7 +17,7 @@ if (!String.prototype.padStart) {
 
 export const displayTime = (seconds) => {
   const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds - (seconds * minutes);
+  const remainingSeconds = seconds - (60 * minutes);
   const remainingSecondsPadded = remainingSeconds.toString().padStart(2, '0');
 
   return `${minutes}:${remainingSecondsPadded}`;
